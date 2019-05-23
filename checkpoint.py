add1638f77a1778a0c45f175e8997ee9baae52a3
Sonali Singh <singhsonali0503@gmail.com>
2019-05-23 15:28:29 -0400

    added support for post-training quantization and validation of any model

--- checkpoint.py
+++ checkpoint.py
@@ -6,26 +6,24 @@ from numbers import Number
 from tabulate import tabulate
 import torch
 
-def save_checkpoint(epoch, arch, model, best_prec1,optimizer=None, dir='.'):
+def save_checkpoint(epoch, arch, model, best_prec1, optimizer=None, dir='.', filename=None):
     """Save a pytorch training checkpoint
 
     Args:
         epoch: current epoch number
         arch: name of the network architecture/topology
         model: a pytorch model
+        best_prec1: top1 acc of the model
         optimizer: the optimizer used in the training session
-        scheduler: the CompressionScheduler instance used for training, if any
-        extras: optional dict with additional user-defined data to be saved in the checkpoint.
-            Will be saved under the key 'extras'
-        is_best: If true, will save a copy of the checkpoint with the suffix 'best'
-        name: the name of the checkpoint file
         dir: directory in which to save the checkpoint
+        filename: the name of the checkpoint file
     """
     if not os.path.isdir(dir):
         raise IOError(ENOENT, 'Checkpoint directory does not exist at', os.path.abspath(dir))
 
     str_val = '%.3f'%best_prec1
-    filename = 'checkpoint_' + arch + '_' + str(str_val) + 'pth.tar'
+    if not filename:
+        filename = 'checkpoint_' + str(str_val) + '.pth.tar'
     fullpath = os.path.join(dir, filename)
     print("Saving checkpoint to: {}".format(fullpath))
 
@@ -34,6 +32,7 @@ def save_checkpoint(epoch, arch, model, best_prec1,optimizer=None, dir='.'):
     checkpoint['arch'] = arch
     checkpoint['best_prec1'] = best_prec1
     checkpoint['state_dict'] = model.state_dict()
+    print('model state dict: {}'.format(model.state_dict()['fc.weight'].size() ))
     if optimizer is not None:
         checkpoint['optimizer_state_dict'] = optimizer.state_dict()
         checkpoint['optimizer_type'] = type(optimizer)
@@ -54,29 +53,29 @@ def get_contents_table(d):
     return tabulate(contents, headers=["Key", "Type", "Value"], tablefmt="fancy_grid")
 
 
-def load_checkpoint(model, chkpt_file, optimizer=None, model_device=None, *, lean_checkpoint=False):
+def load_checkpoint(model, chkpt_file, optimizer=None, model_device=None):
     """Load a pytorch training checkpoint.
 
     Args:
         model: the pytorch model to which we will load the parameters
         chkpt_file: the checkpoint file
-        lean_checkpoint: if set, read into model only 'state_dict' field
         optimizer: [deprecated argument]
         model_device [str]: if set, call model.to($model_device)
                 This should be set to either 'cpu' or 'cuda'.
-    :returns: updated model, compression_scheduler, optimizer, start_epoch
+    :returns: updated model, optimizer, start_epoch, best_prec1, arch
     """
     if not os.path.isfile(chkpt_file):
         raise IOError(ENOENT, 'Could not find a checkpoint file at', chkpt_file)
 
     print("=> loading checkpoint %s", chkpt_file)
-    checkpoint = torch.load(chkpt_file, map_location=lambda storage, loc: storage)
+    #checkpoint = torch.load(chkpt_file, map_location=lambda storage, loc: storage)
+    checkpoint = torch.load(chkpt_file)
+    print('checkpoint contents'.format(checkpoint.keys()))
 
     print('=> Checkpoint contents:\n{}\n'.format(get_contents_table(checkpoint)))
     if 'state_dict' not in checkpoint:
         raise ValueError("Checkpoint must contain the model parameters under the key 'state_dict'")
 
-    #checkpoint_epoch = checkpoint.get('epoch', None)
     start_epoch = checkpoint['epoch']
 
     model.load_state_dict(checkpoint['state_dict'])
--- main.py
+++ main.py
@@ -4,20 +4,77 @@ import time
 import torch
 import torch.nn as nn
 import numpy as np
-from mobilenet_trimmed1 import MobileNet_trimmed
+from mobilenet_trimmed1 import *
 from data_loaders import tinyimagenet_get_datasets
 from train_routines import *
 from checkpoint import *
+from utils import quantize_model
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+parser = argparse.ArgumentParser(description='Separable CNN training using tiny imagenet and pytorch')
+parser.add_argument("-d", "--dataset", default='tiny-imagenet-200', \
+        choices=['tiny-imagenet-200'], \
+        help="dataset to train on (default: tiny-imagenet-200)")
 
-def main(data_dir, epochs, arch, model_path=None, resume=False, chkpt_file=None):
-    batch_size = 64
+parser.add_argument("--data-dir", default='./datasets/tinyimagenet/tiny-imagenet-200', \
+        help='path to dataset (default: ./datasets/tinyimagenet/tiny-imagenet-200)')
+
+parser.add_argument("--batch-size", default=64, type=int, \
+        help='batch size (default: 64)')
+
+parser.add_argument("--epochs", default=25, type=int, \
+        help='total number of epochs to train for (default: 25)')
+
+parser.add_argument("--arch", default='mobilenet_trimmed1', \
+        choices=['mobilenet_trimmed1', 'mobilenet_trimmed1_drop'], \
+        help='model to train (default: mobilenet_trimmed1)')
+
+parser.add_argument("--lr", default=0.01, type=float, \
+        help='learning rate (default: 0.01)')
+
+parser.add_argument('--topk', default=1, type=int, \
+        help='top-k accuracy (default: 1)')
+
+parser.add_argument("--chkpt-dir", default='saved_models', \
+        help='directory for saving checkpoints (default: saved_models)')
+
+parser.add_argument("--chkpt-file", default='checkpoint.pth.tar', \
+        help='checkpoint file name (default: checkpoint.pth.tar)')
+
+parser.add_argument("--resume", action='store_true', \
+        help='resumes training from checkpoint (default: False)')
+
+#parser.add_argument("--mode", default='train', \
+#        choices=['train', 'quantize', 'validate'], \
+#        help='(default: False)')
+
+parser.add_argument("--quantize", action='store_true', \
+        help='quantize weights of a pre-trained model (default: False)')
+
+parser.add_argument("--I", type=int, default=2, \
+        help='fixed point integer part')
+
+parser.add_argument("--F", type=int, default=4, \
+        help='fixed point fractional part')
+
+parser.add_argument("--train", action='store_true', \
+        help='train a model (default: False)')
+
+parser.add_argument("--validate", action='store_true', \
+        help='test a model (default: False)')
+
+
+def main(args):
+    batch_size = args.batch_size
     start_epoch = 0
 
-    train_dataset, val_dataset = tinyimagenet_get_datasets(data_dir)
+    # prepare dataset
+    train_dataset, val_dataset = None, None
+    if args.dataset == 'tiny-imagenet-200':
+        train_dataset, val_dataset = tinyimagenet_get_datasets(args.data_dir)
 
+    # load dataset
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
@@ -26,32 +83,61 @@ def main(data_dir, epochs, arch, model_path=None, resume=False, chkpt_file=None)
     else:
         print('GPU device not available')
 
-    model = MobileNet_trimmed().to(device)
+    # instantiate the model class
+    model = None
+    if args.arch == 'mobilenet_trimmed1':
+        model = MobileNet_trimmed()
+    elif args.arch == 'mobilenet_trimmed1_drop':
+        model = MobileNet_trimmed_dropout()
+
+    # transfer to device of choice
+    if args.train or args.validate:
+        model = model.to(device)
     print(model)
 
     # Loss fn and optimizer
     criterion = nn.CrossEntropyLoss()
     # TODO: add an lr scheduler
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    best_prec1 = 0
-    if resume:
-        chkpt = os.path.join(model_path, chkpt_file)
-        model, optimizer, start_epoch, best_prec1, arch = load_checkpoint(model, chkpt, optimizer, device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+    best_prec1, avg_prec1 = 0, 0
 
-    for epoch in range(start_epoch, start_epoch+epochs):
-        # train for current epoch
-        train(train_loader, model, criterion, optimizer, epoch, device)
+    chkpt_dir = os.path.join(args.chkpt_dir, args.arch)
+    if args.resume or args.validate:
+        if not os.path.exists(chkpt_dir):
+            os.makedirs(chkpt_dir)
 
+        chkpt_file = os.path.join(chkpt_dir, args.chkpt_file)
+        if not os.path.exists(chkpt_file):
+            print('No checkpoint file {}'.format(chkpt_file))
+
+        #chkpt = os.path.join(model_path, chkpt_file)
+        model, optimizer, start_epoch, best_prec1, arch = load_checkpoint(model, chkpt_file, optimizer, device)
+
+    if args.train:
+        for epoch in range(start_epoch, start_epoch+args.epochs):
+            # train for current epoch
+            train(train_loader, model, criterion, optimizer, epoch, device)
+
+            # validate
+            avg_prec1 = validate(val_loader, model, criterion, epoch, device)
+
+            if avg_prec1 > best_prec1:
+                best_prec1 = avg_prec1
+
+                # Checkpoint the model
+                save_checkpoint(epoch, args.arch, model, best_prec1, optimizer, chkpt_dir)
+        save_checkpoint(start_epoch+args.epochs, args.arch, model, avg_prec1, optimizer, chkpt_dir)
+
+    if args.quantize:
+        quantize_model(model, chkpt_dir, args.chkpt_file, device, args.I, args.F)
+
+    if args.validate:
         # validate
-        avg_prec1 = validate(val_loader, model, criterion, epoch, device)
+        #model.load_state_dict(torch.load(model_path))
+        avg_prec1 = validate(val_loader, model, criterion, 0, device)
 
-        if avg_prec1 > best_prec1:
-            best_prec1 = avg_prec1
-
-            # Checkpoint the model
-            save_checkpoint(epoch, arch, model, best_prec1, optimizer, model_path)
 
 if __name__ == '__main__':
-    #main('./datasets/tinyimagenet/tiny-imagenet-200', 2, 'mobilenet_trimmed1', './saved_models')
-    main('./datasets/tinyimagenet/tiny-imagenet-200', 18, 'mobilenet_trimmed1', './saved_models', True, 'checkpoint_mobilenet_trimmed1_59.375pth.tar')
-
+    args = parser.parse_args()
+    print('Parameters: {}'.format(args))
+    main(args)
